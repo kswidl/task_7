5afe3ca509e13f9e663d48ea36d6fdbc968f7eb7
kswidl <kitabakim@gmail.com>
2026-05-09 03:07:36 +0300

    add multiple independent listeners

--- lab_7.js
+++ lab_7.js
@@ -6,6 +6,15 @@ bus.on("message", (data) => {
     console.log(`User received message: ${data.text}`);
 });
 
+bus.on("message", (data) => {
+    console.log(`Logger saved message from ${data.sender}`);
+});
+
+bus.on("message", (data) => {
+    console.log(`Notification sent about new message`);
+});
+
 bus.emit("message", {
+    sender: "Alice",
     text: "Hello from another entity!"
 });
